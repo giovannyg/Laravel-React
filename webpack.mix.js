c371d47d1feb7428435534379538d1bf89bfb15e
giovanny <giovanny@messoft.com>
2020-09-07 17:56:39 -0500

    Struggling with JQuery

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.react('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.react('resources/js/admin.js', 'public/js')
+    .sass('resources/sass/admin.scss', 'public/css');
+
+
+mix.browserSync('http://laravel.web');
+
+if(mix.inProduction()) {
+  mix.version();
+}
